dfcdbdb298c11af5355d3d17761a06cc1a412f1c
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-24 15:24:48 -0700

    add functionality to convert 5 more foreign currencies.

--- src/exchange.js
+++ src/exchange.js
@@ -22,13 +22,23 @@ export class Exchange {
     if (this.select === "AUD") {
       this.changedAus = (this.usd * this.australia.toFixed());
     } else if (this.select === "ARS") {
-      this.changedArg = (this.usd * this.argentine.toFixed())
+      this.changedArg = (this.usd * this.argentine.toFixed());
     } else if (this.select === "BGN") {
-      this.changedBul = (this.usd * this.bulgarian.toFixed())
+      this.changedBul = (this.usd * this.bulgarian.toFixed());
     } else if (this.select === "COP") {
-      this.changedCop = (this.usd * this.colombia.toFixed())
+      this.changedCop = (this.usd * this.colombia.toFixed());
     } else if (this.select === "EUR") {
-      this.changedEur = (this.usd * this.europe.toFixed())
+      this.changedEur = (this.usd * this.europe.toFixed());
+    } else if (this.select === "DKK") {
+      this.changedDan = (this.usd * this.danish.toFixed());
+    } else if (this.select === "HUF") {
+      this.changedHun = (this.usd * this.hungarian.toFixed());
+    } else if (this.select === "IDR") {
+      this.changedInd = (this.usd * this.indonesian.toFixed());
+    } else if (this.select === "KRW") {
+      this.changedKor = (this.usd * this.southKorean.toFixed());
+    } else if (this.select === "PYG") {
+      this.changedPar = (this.usd * this.paraguayan.toFixed());
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -20,6 +20,11 @@ $(document).ready(function () {
       exchange.bulgarian = response.conversion_rates.BGN;
       exchange.colombia = response.conversion_rates.COP;
       exchange.europe = response.conversion_rates.EUR;
+      exchange.danish = response.conversion_rates.DKK;
+      exchange.hungarian = response.conversion_rates.HUF;
+      exchange.indonesian = response.conversion_rates.IDR;
+      exchange.southKorean = response.conversion_rates.KRW;
+      exchange.paraguayan = response.conversion_rates.PYG;
       exchange.change(response);
       if (exchange.select === "AUD") {
         $("#money").html(exchange.usd + " USD " + " is " + exchange.changedAus + " Australian currency.");
@@ -31,6 +36,16 @@ $(document).ready(function () {
         $("#money").html(exchange.usd + " USD " + " is " + exchange.changedCop + " Colombian currency.");
       } else if (exchange.select === "EUR") {
         $("#money").html(exchange.usd + " USD " + " is " + exchange.changedEur + " European currency.");
+      } else if (exchange.select === "DKK") {
+        $("#money").html(exchange.usd + " USD " + " is " + exchange.changedDan + " Danish currency.");
+      } else if (exchange.select === "HUF") {
+        $("#money").html(exchange.usd + " USD " + " is " + exchange.changedHun + " Hungarian currency.");
+      } else if (exchange.select === "IDR") {
+        $("#money").html(exchange.usd + " USD " + " is " + exchange.changedInd  + " Indonesian currency.");
+      } else if (exchange.select === "KRW") {
+        $("#money").html(exchange.usd + " USD " + " is " + exchange.changedKor + " South Korean currency.");
+      } else if (exchange.select === "PYG") {
+        $("#money").html(exchange.usd + " USD " + " is " + exchange.changedPar + " Paraguayan currency.");
       }
     })();
   });
